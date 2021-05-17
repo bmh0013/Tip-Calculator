781e4480b46c90c8ee417ae3f66415bf135db812
Brandon Harden <bmh0013@gmail.com>
2021-05-17 11:50:25 -0500

    updated functionality

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
+  Keyboard,
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
 } from "react-native";
 
 export default function App() {
   const [total, onChangeTotal] = React.useState(null);
-  const [tip, onChangeTip] = React.useState(.20);
+  const [tip, onChangeTip] = React.useState(null);
   const [customTip, onChangeCustomTip] = React.useState(null);
 
-  const TipButton = ({ percent, total, tip }) => {
+  const TipButton = ({ percent, isCustom = false }) => {
     let result = total * percent;
     result = Math.round(result * 100) / 100;
     return (
@@ -21,40 +23,42 @@ export default function App() {
       style={percent === tip ? styles.active : styles.button}
       onPress={() => {onChangeTip(percent)}}
       >
-        <Text>{percent * 100}%</Text>
+        {!isCustom ? <Text>{percent * 100}%</Text> : <Text>Custom %</Text>}
         <Text>${result}</Text>
       </TouchableOpacity>
     );
   };
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>How much was the bill?</Text>
-        <TextInput
-          style={styles.input}
-          value={total}
-          keyboardType="decimal-pad"
-          onChangeText={onChangeTotal}
-        />
-        <Text>Enter Custom Tip</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="number-pad"
-          onChangeText={(text) => onChangeCustomTip(text)}
-        />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View>
+          <Text>How much was the bill?</Text>
+          <TextInput
+            style={styles.input}
+            value={total}
+            keyboardType="decimal-pad"
+            onChangeText={onChangeTotal}
+          />
+          <Text>Enter Custom Tip</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="number-pad"
+            onChangeText={(text) => onChangeCustomTip(text)}
+          />
+        </View>
+        <View style={styles.tipContainer}>
+          <TipButton percent={0.1} />
+          <TipButton percent={0.15} />
+          <TipButton percent={0.2} />
+          <TipButton percent={customTip * .01} isCustom={true} />
+        </View>
+        <View style={styles.resultContainer}>
+          <Text>Grand Total: ${ +total + (+total * +tip) }</Text>
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.tipContainer}>
-        <TipButton percent={0.1} total={total} tip={tip} onPress={() => onChangeTip(tip)} />
-        <TipButton percent={0.15} total={total} tip={tip} onPress={() => onChangeTip(tip)} />
-        <TipButton percent={0.2} total={total} tip={tip} onPress={() => onChangeTip(tip)} />
-      </View>
-      <View style={styles.resultContainer}>
-        <Text>Grand Total: ${!customTip ? +total + (+total * +tip) : +total + (+total * (+customTip * .01) )}</Text>
-      </View>
-      {tip === 'custom' && Text}
-      <StatusBar style="auto" />
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
